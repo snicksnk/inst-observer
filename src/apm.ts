e029b5e4e6526bcb4e9409587ff33130d252b554
Administrator <admin@example.com>
2022-02-15 17:21:00 +0000

    Update apm.ts

--- src/apm.ts
+++ src/apm.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const apm = require('elastic-apm-node').start({
   // active: false,
-  serviceName: 'IGTG',
+  serviceName: 'S',
   environment: process.env.APP_ENV || 'develop',
   serverUrl: process.env.APM_URL,
 });
