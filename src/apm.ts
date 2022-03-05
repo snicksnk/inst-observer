e82af081d45a40f3094f41359bcb6b5338b19019
Yuri Taratorkin <snicksnk@gmail.com>
2022-03-05 09:18:42 +0000

    Update apm.ts

--- src/apm.ts
+++ src/apm.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const apm = require('elastic-apm-node').start({
-  active: false,
+  // active: false,
   serviceName: 'IGTG',
   environment: process.env.APP_ENV || 'develop',
   serverUrl: process.env.APM_URL,
